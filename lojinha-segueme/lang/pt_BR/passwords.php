6f8e3bc4b10cef9a7b1fbdfda47bc75855e64de0
Wober Silva <wobersaojose@gmail.com>
2026-01-27 20:15:47 -0300

    Add Portuguese translations for auth, validation, passwords and pagination

---
+++ lojinha-segueme/lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida com sucesso.',
+    'sent' => 'Enviamos um link para redefinir sua senha por e-mail.',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => 'Não encontramos um usuário com este endereço de e-mail.',
+
+];
